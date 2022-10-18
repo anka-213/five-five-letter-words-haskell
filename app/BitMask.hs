484d975446e68151f83bd1e53c523b62cf424c62
Andreas Källberg <anka.213@gmail.com>
2022-10-18 19:59:03 +0200

    Speed: replace IntSet with custom BitMask type
    Down to 0.843s
    No print: 0.640s

---
+++ app/BitMask.hs
@@ -0,0 +1,44 @@
+module BitMask where
+import Data.Word (Word32)
+import qualified Data.IntMap.Strict as IntMap
+import Data.Bits
+import Utils.Containers.Internal.BitUtil
+
+
+-- | A simpler IntSet for numbers less than 32
+newtype Bitmask32 = BM {unBM :: Word32}
+  deriving (Eq, Ord)
+
+empty :: Bitmask32
+empty = BM 0
+
+insert :: IntMap.Key -> Bitmask32 -> Bitmask32
+insert k (BM m) | k >= 32 || k < 0 = error $ "BitMask.insert: out of range: " ++ show k
+insert k (BM m) = BM $ setBit m k
+
+fromList :: [Int] -> Bitmask32
+fromList = foldr insert empty
+
+disjoint :: Bitmask32 -> Bitmask32 -> Bool
+disjoint (BM a) (BM b) = a .&. b == 0
+
+-- >>> BitMask.minimum $ fromList [5,7]
+-- >>> BitMask.minimum $ fromList []
+-- Just 5
+-- Nothing
+minimum :: Bitmask32 -> Maybe Int
+minimum = adjustThing . countTrailingZeros . unBM
+
+adjustThing :: Int -> Maybe Int
+adjustThing 32 = Nothing
+adjustThing n = Just n
+
+notMember :: IntMap.Key -> Bitmask32 -> Bool
+notMember k (BM b) = not $ testBit b k
+
+size :: Bitmask32 -> Int
+size = popCount . unBM
+
+union :: Bitmask32 -> Bitmask32 -> Bitmask32
+union (BM a) (BM b) = BM (a .|. b)
+
--- app/FindCombinations.hs
+++ app/FindCombinations.hs
@@ -14,23 +14,23 @@ import Data.IntMap (IntMap)
 import Data.Char (ord, toLower)
 import GHC.Char (chr)
 import Control.Applicative (Alternative((<|>)))
+import qualified BitMask
+import BitMask (Bitmask32)
 
-type SomeWord = IntSet
-
-findThings :: IntSet -> IntMap ([] SomeWord) -> [[IntSet]]
-findThings usedLetters remaining | IntSet.size usedLetters >= 5*5 = pure []
+findThings :: Bitmask32 -> IntMap [Bitmask32] -> [[Bitmask32]]
+findThings usedLetters remaining | BitMask.size usedLetters >= 5*5 = pure []
 findThings usedLetters remaining = do
-    let relevantParts = IntMap.filterWithKey (\k v -> k `IntSet.notMember` usedLetters) remaining
+    let relevantParts = IntMap.filterWithKey (\k v -> k `BitMask.notMember` usedLetters) remaining
     Just ((k, firstUnused), otherUnused) <- pure $ IntMap.minViewWithKey relevantParts
     (usedLetters', firstOrSecondUnused) <-
         pure (usedLetters, firstUnused) <|> do
-            guard $ IntSet.size usedLetters `mod` 5 == 0 -- If we haven't skipped any letters so far
+            guard $ BitMask.size usedLetters `mod` 5 == 0 -- If we haven't skipped any letters so far
             Just (secondUnused, otherUnused') <- pure $ IntMap.minView otherUnused
-            pure (IntSet.insert k usedLetters, secondUnused)
+            pure (BitMask.insert k usedLetters, secondUnused)
     attempt <- firstOrSecondUnused
-    guard $ IntSet.disjoint attempt usedLetters'
+    guard $ BitMask.disjoint attempt usedLetters'
 
-    rest <- findThings (IntSet.union attempt usedLetters') otherUnused -- Should only use later than attempt
+    rest <- findThings (BitMask.union attempt usedLetters') otherUnused -- Should only use later than attempt
     return $ attempt : rest
 
 charToInt :: Char -> Int
@@ -47,13 +47,13 @@ main = do
     let words5 =
             [ (ws, w)
             | w <- words , length w == 5
-            , let ws = IntSet.fromList (map charToInt w) , IntSet.size ws == 5
+            , let ws = BitMask.fromList (map charToInt w) , BitMask.size ws == 5
             ]
     let words5Map = Set.toList <$> IntMap.fromListWith Set.union
             [ (leastCommonLetter, Set.singleton ws)
             | (ws, w) <- words5
-            , let Just (leastCommonLetter, _) = IntSet.minView ws]
+            , let Just leastCommonLetter = BitMask.minimum ws]
     let reverseMap = Map.fromListWith (++) $ fmap (fmap pure) words5
-    let result = traverse (reverseMap Map.!) =<< findThings IntSet.empty words5Map
+    let result = traverse (reverseMap Map.!) =<< findThings BitMask.empty words5Map
     mapM_ print result
     print $ length result
